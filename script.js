6cb19c9e6ad60c02ef2ef4c846c04e8e457e238d
RUNAaa0712 <98579097+tjc-kousei@users.noreply.github.com>
2022-07-07 22:51:42 +0900

    second

--- script.js
+++ script.js
@@ -31,7 +31,40 @@ fetch(endpoint)
         let tr = document.createElement("tr")
         for(let i=0; i<name.length; i++) {
             const td = document.createElement("td")
+            td.id = `table[${index}][${i}]`
             td.innerHTML = obj[name[i]] //name[i]でkey指定してvalueを取り出す
+            if(i==name.length-1) {
+                td.addEventListener("mouseover", ()=> {
+                    td.classList.toggle("hover")
+                    set_row_color(index,i)
+                    set_row_color(index,i,"set")
+                })
+                td.addEventListener("mouseout", ()=> {
+                    td.classList.toggle("hover")
+                    set_row_color(index,i,"remove")
+                })
+                td.addEventListener("click", ()=> {
+                    navigator.clipboard.writeText(td.innerHTML)
+                    .then()
+                    .catch(err => console.log("Copy Error...",err) )
+                })
+                function set_row_color(index,num,type){
+                    if(type=="set") {
+                        for(let i=0;i<name.length;i++) {
+                            if(i==num) continue
+                            const ele = document.getElementById("table"+`[${index}][${i}]`)
+                            ele.style.backgroundColor = "rgba(255,0,0,.5)"
+                            ele.style.color = "white"
+                        }
+                    } else if(type=="remove") {
+                        for(let i=0;i<name.length;i++) {
+                            if(i==num) continue
+                            const ele = document.getElementById("table"+`[${index}][${i}]`)
+                            ele.style = ""
+                        }
+                    }
+                }
+            }
             //log.innerHTML += obj[name[i]]
             tr.appendChild(td)
         }
